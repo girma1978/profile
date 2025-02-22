0e66c76763a257313966edd3b53f3e6ae0c7cf0f
Lakulength@1717 <girm2002@gmail.com>
2025-02-22 15:07:20 -0500

    navbar decorected

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import AboutMe from './components/AboutMe';
-// import Portfolio from './components/Portfolio';
-// import Contact from './components/Contact';
-// import Resume from './components/Resume';
-import './App.css';
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Header />
-//         <main>
-//           <Routes>
-//             <Route path="/about-me" element={<AboutMe />} /> {/* Route for About me */}
-//             <Route path="/portfolio" element={<Portfolio />} />
-//             <Route path="/contact" element={<Contact />} />
-//             <Route path="/resume" element={<Resume />} />
-//             <Route path="/" element={<AboutMe />} /> {/* Default route */}
-//           </Routes>
-//         </main>
-//         <Footer />
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AboutMe from './components/AboutMe';  // Adjust path if necessary
 import Portfolio from './components/Portfolio';  // Example for another page (Portfolio)
 import Contact from './components/Contact';  // Example for Contact page
 import Resume from './components/Resume';  // Example for Resume page
 import Navigation from './components/Navigation';  // Your Navigation component
-
+import './App.css';
 const App = () => {
   return (
     <Router>
