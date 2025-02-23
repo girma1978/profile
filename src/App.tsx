bddeaf00083c19be2e1e7764c2a9f7b771861e63
Lakulength@1717 <girm2002@gmail.com>
2025-02-23 01:32:12 -0500

    meadia query updated again

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,3 @@
-// import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import AboutMe from './components/AboutMe';
-// import Portfolio from './components/Portfolio';
-// import Contact from './components/Contact';
-// import Resume from './components/Resume';
-// import './App.css';
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Header />
-//       <Routes>
-//         <Route path="/profile" element={<AboutMe />} />
-//         <Route path="/portfolio" element={<Portfolio />} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/resume" element={<Resume />} />
-//       </Routes>
-//       <Footer />
-//     </Router>
-//   );
-// };
-
-// export default App;
 
 
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
