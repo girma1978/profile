35a0f6389fc1f092ca25cac045fd5c63b3ad7767
Lakulength@1717 <girm2002@gmail.com>
2025-02-23 01:14:13 -0500

    narbar modified

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,7 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
-        {/* Redirect any /profile path to /about-me */}
-        <Route path="/profile/*" element={<Navigate to="/about-me" replace />} />
-        
-        {/* Routes for other paths */}
-        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/profile" element={<AboutMe />} />
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/resume" element={<Resume />} />
